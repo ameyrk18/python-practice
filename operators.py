1932960119450c73852305585a4eef4d7208bea8
ameyrk18 <ameyrk18@gmail.com>
2018-01-10 23:05:44 +0000

    Operators checked in

---
+++ operators.py
@@ -0,0 +1,11 @@
+## Arithmetic operators
+
+num1=2
+num2=3
+print("num1+num2=",num1+num2)
+print("num1-num2=",num1-num2)
+print("num1/num2=",num1/num2)
+print("num1*num2=",num1*num2)
+print("num1%num2=",num1%num2)
+print("2^3=",num1**num2)
+print("22//7=",22//7)

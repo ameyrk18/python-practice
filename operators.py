816abdba4501844b39d169a45b0a161a77cc8403
ameyrk18 <ameyrk18@gmail.com>
2018-01-11 06:51:26 +0000

    Operators checked in

--- operators.py
+++ operators.py
@@ -1,11 +1,44 @@
-## Arithmetic operators
+########## Arithmetic operators ##########
 
+print("########## Arithmetic operators ##########")
 num1=2
 num2=3
+# Addition
 print("num1+num2=",num1+num2)
+# Substract
 print("num1-num2=",num1-num2)
+# Division
 print("num1/num2=",num1/num2)
+# Multiplication
 print("num1*num2=",num1*num2)
+# Modulus (Gives you the reminder)
 print("num1%num2=",num1%num2)
+# Exponential
 print("2^3=",num1**num2)
+# Float (Rounding off the decimal part)
 print("22//7=",22//7)
+
+########## Assignment operators ##########
+
+print ("########## Assignment operators ##########")
+num3 = num1 + num2
+print(num3)
+num3+=num2
+print(num3)
+x=5
+x/=5
+print(x)
+x=5
+x%=5
+print(x)
+x=5
+x //=2
+print(x)
+x=5
+x**=x
+print(x)
+
+
+########## Comparison operators ##########
+
+print("########## Comparison operators ##########")

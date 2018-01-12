09ba14bedc7b5b7e8edea14ecad7e62f833ac39e
ameyrk18 <ameyrk18@gmail.com>
2018-01-12 22:34:37 +0000

    Strings checked in

--- strings.py
+++ strings.py
@@ -1,4 +1,24 @@
 string1="Star"
 string2="Wars"
 
-# String concat
+# String concatination
+print("# String concatination")
+print("tring1 + String2", string1+string2)
+
+# String repitation
+print("# String repitation")
+print("String1",(string1+string2)*5)
+
+# String slicing
+
+print("# String slicing")
+print(string1[0:2])
+
+print(string1[3])
+print(string1[-1])
+
+#type specific method
+
+print(string1.count('s',0,3))
+
+print(string1.isalnum())

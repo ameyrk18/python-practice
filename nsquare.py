6b4dab7f2fffdd92cf569368376253a15b3de7e4
ameyrk18 <ameyrk18@gmail.com>
2018-01-14 19:13:39 +0000

    n square checked in

--- nsquare.py
+++ nsquare.py
@@ -1,3 +1,6 @@
+import time
+from random import  randrange
+
 def findmin(alist):
     overallmin=alist[0]
     for i in alist:
@@ -9,4 +12,19 @@ def findmin(alist):
             overallmin=i
     return overallmin
 
-print(findmin(alist=[5,4,2,1,0]))
+#print(findmin(alist=[5,4,2,1,0]))
+#print(findmin(alist=[0,4,2,1,5]))
+
+'''
+def findmin(alist):
+    minsofar=alist[0]
+'''
+
+
+for listsize in range(1000,10001,1000):
+    alist=[randrange(100000) for x in range(listsize)]
+    #print(alist)
+    start=time.time()
+    print(findmin(alist))
+    end=time.time()
+    print("Size: %d time: %f" %(listsize,end-start))

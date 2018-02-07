7ea134ca65c11eae411fbc7eefc8ba1eec3ad77e
ameyrk18 <ameyrkamble@gmail.com>
2018-02-07 07:09:56 +0000

    Adding type conversions

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-for i in range(1000,10000,1000):
-        print(i)
+# for i in range(1000,10000,1000):
+#        print(i)
+
+
+#print(2/1)
+
+import redis
+r = redis.StrictRedis(host='192.168.99.100', port=6379, db=0)
+for key in r.scan_iter("key_pattern*"):
+    print (key)
